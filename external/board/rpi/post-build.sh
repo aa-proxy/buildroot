490cb13170a6e132895db57f52138e4d35e23cfa
Mariusz Bialonczyk <manio@skyboo.net>
2025-07-18 12:48:30 +0200

    configs, board scripts: adjust to new modified path

--- external/board/rpi/post-build.sh
+++ external/board/rpi/post-build.sh
@@ -7,4 +7,4 @@ set -x
 mv ${TARGET_DIR}/etc/aawgd.conf ${BINARIES_DIR}/aawgd.conf
 ln -sf /boot/aawgd.conf ${TARGET_DIR}/etc/aawgd.conf
 
-source board/raspberrypi/post-build.sh
+source board/rpi/post-build.sh
