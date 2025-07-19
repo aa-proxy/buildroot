a86bb1d7dbbc58074d858af01034e960f336d4cc
Mariusz Bialonczyk <manio@skyboo.net>
2025-07-19 07:02:18 +0200

    aa-proxy-rs.mk: use RUSTC_TARGET_NAME when installing into target
    This helps compiling for different architectures

--- external/package/aa-proxy-rs/aa-proxy-rs.mk
+++ external/package/aa-proxy-rs/aa-proxy-rs.mk
@@ -2,7 +2,7 @@ AA_PROXY_RS_VERSION = 0.6.0
 AA_PROXY_RS_SITE = $(call github,manio,aa-proxy-rs,v$(AA_PROXY_RS_VERSION))
 
 define AA_PROXY_RS_INSTALL_TARGET_CMDS
-    $(INSTALL) -D -m 0755 $(@D)/target/armv7-unknown-linux-gnueabihf/release/aa-proxy-rs  $(TARGET_DIR)/usr/bin
+    $(INSTALL) -D -m 0755 $(@D)/target/$(RUSTC_TARGET_NAME)/release/aa-proxy-rs $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(cargo-package))
