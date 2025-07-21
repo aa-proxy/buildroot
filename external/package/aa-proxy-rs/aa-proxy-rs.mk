a63c86876000850c2b1e3bd1bc06e3c5b5397542
Mariusz Bialonczyk <manio@skyboo.net>
2025-07-21 16:46:01 +0200

    aa-proxy-rs: bump version

--- external/package/aa-proxy-rs/aa-proxy-rs.mk
+++ external/package/aa-proxy-rs/aa-proxy-rs.mk
@@ -1,5 +1,5 @@
-AA_PROXY_RS_VERSION = 0.6.0
-AA_PROXY_RS_SITE = $(call github,manio,aa-proxy-rs,v$(AA_PROXY_RS_VERSION))
+AA_PROXY_RS_VERSION = 6bcc2cf3df3a8c4f0a375c10352d410a86c94330
+AA_PROXY_RS_SITE = $(call github,aa-proxy,aa-proxy-rs,$(AA_PROXY_RS_VERSION))
 
 define AA_PROXY_RS_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0755 $(@D)/target/$(RUSTC_TARGET_NAME)/release/aa-proxy-rs $(TARGET_DIR)/usr/bin
