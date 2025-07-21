2cbd926e8900b64202ba75e66e5f65d1854619d7
Mariusz Bialonczyk <manio@skyboo.net>
2025-07-21 17:45:31 +0200

    aa-proxy-rs: install default config file

--- external/package/aa-proxy-rs/aa-proxy-rs.mk
+++ external/package/aa-proxy-rs/aa-proxy-rs.mk
@@ -3,6 +3,7 @@ AA_PROXY_RS_SITE = $(call github,aa-proxy,aa-proxy-rs,$(AA_PROXY_RS_VERSION))
 
 define AA_PROXY_RS_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0755 $(@D)/target/$(RUSTC_TARGET_NAME)/release/aa-proxy-rs $(TARGET_DIR)/usr/bin
+    $(INSTALL) -D -m 0644 $(@D)/contrib/config.toml $(TARGET_DIR)/etc/aa-proxy-rs/config.toml
 endef
 
 $(eval $(cargo-package))
