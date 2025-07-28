4747060a7af45a1edf84323c5f051c5d808904f9
Mariusz Bialonczyk <manio@skyboo.net>
2025-07-28 11:08:55 +0200

    qemu: fix fstab entries for virtio/vda

--- external/board/qemu/post-build.sh
+++ external/board/qemu/post-build.sh
@@ -15,3 +15,12 @@ fi
 auto eth0
 iface eth0 inet dhcp
 EOF
+
+# Modify /etc/fstab entries
+if [ -e ${TARGET_DIR}/etc/fstab ]; then
+    # Remove the /dev/mmcblk0p1 line
+    sed -i '/^[[:space:]]*\/dev\/mmcblk0p1[[:space:]]/d' ${TARGET_DIR}/etc/fstab
+
+    # Replace /dev/mmcblk0p3 with /dev/vda2
+    sed -i 's|^/dev/mmcblk0p3|/dev/vda2|' ${TARGET_DIR}/etc/fstab
+fi
