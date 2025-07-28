afd6d1f20e0c0899726c99a5c21f4674696dc621
Mariusz Bialonczyk <manio@skyboo.net>
2025-07-28 11:05:25 +0200

    qemu: ensure proper network access on eth0

--- external/board/qemu/post-build.sh
+++ external/board/qemu/post-build.sh
@@ -9,3 +9,9 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # QEMU graphical window' ${TARGET_DIR}/etc/inittab
 fi
+
+# Ensure proper /etc/network/interfaces for qemu network access
+    cat << EOF > ${TARGET_DIR}/etc/network/interfaces
+auto eth0
+iface eth0 inet dhcp
+EOF
