ab4782a78c01796fb430dfda40eac50c186b0679
Mariusz Bialonczyk <manio@skyboo.net>
2025-08-04 21:57:03 +0200

    rpi0w: add workaround for build with 32-bit kernel name
    Bug reported upstream:
    https://gitlab.com/buildroot.org/buildroot/-/issues/131

--- external/board/rpi/post-image.sh
+++ external/board/rpi/post-image.sh
@@ -4,4 +4,10 @@ set -e
 
 ${BR2_EXTERNAL_AA_PROXY_OS_PATH}/scripts/generate-issue.sh
 
+# workaround for:
+# https://gitlab.com/buildroot.org/buildroot/-/issues/131
+if grep -q 'rpi0w' "${BR2_CONFIG}"; then
+    cp ${BUILD_DIR}/linux-custom/arch/arm/boot/zImage ${BUILD_DIR}/../images/Image
+fi
+
 source board/raspberrypi/post-image.sh
