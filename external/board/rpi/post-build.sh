af13d02fb7f5f6dba0c146dad616a7059f0e0326
Mariusz Bialonczyk <manio@skyboo.net>
2025-08-08 08:54:48 +0200

    rpi: fix incorrect hook in 12f8ffb – invoke should run in post-build, not post-image

---
+++ external/board/rpi/post-build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -u
+set -e
+
+${BR2_EXTERNAL_AA_PROXY_OS_PATH}/scripts/generate-issue.sh
+
+source board/raspberrypi/post-build.sh
--- external/board/rpi/post-image.sh
+++ external/board/rpi/post-image.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-${BR2_EXTERNAL_AA_PROXY_OS_PATH}/scripts/generate-issue.sh
-
 # workaround for:
 # https://gitlab.com/buildroot.org/buildroot/-/issues/131
 if grep -q 'rpi0w' "${BR2_CONFIG}"; then
