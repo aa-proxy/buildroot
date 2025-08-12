0ad7bbb0cf6c133e42d58a0929b5d70d56bd1e4f
Mariusz Bialonczyk <manio@skyboo.net>
2025-08-12 19:46:51 +0200

    introduce /etc/overlay_init to mount overlay before /sbin/init
    This fixes issues where changes to /etc (e.g. /etc/inittab or /etc/init.d scripts)
    were ignored because /etc was enumerated before overlayfs was mounted.
    
    The overlay is now mounted early via a custom init script, allowing /etc
    to be with overlay changes before BusyBox init scans it.

--- external/board/qemu/post-image.sh
+++ external/board/qemu/post-image.sh
@@ -16,7 +16,7 @@ cat <<-_EOF_ > "${START_QEMU_SCRIPT}"
           -M pc \
           -kernel bzImage \
           -drive file=sdcard.img,if=virtio,format=raw \
-          -append "rootwait root=/dev/vda1 rootrw=/dev/vda2 rootrwoptions=rw,noatime nomodeset console=tty1 console=ttyS0" \
+          -append "rootwait root=/dev/vda1 init=/etc/overlay_init rootrw=/dev/vda2 rootrwoptions=rw,noatime nomodeset console=tty1 console=ttyS0" \
           -net nic,model=virtio \
           -net user,hostfwd=tcp::2022-:22,hostfwd=tcp::8080-:80 \
           -serial stdio
