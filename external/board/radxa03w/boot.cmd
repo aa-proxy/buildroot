a3c18a10bd71175e65d027e6e584b5a34bc40647
PugzAreCute <me@pugzarecute.com>
2025-09-05 19:38:51 +0530

    radxa03w: update kernel init args

--- external/board/radxa03w/boot.cmd
+++ external/board/radxa03w/boot.cmd
@@ -1,7 +1,7 @@
 setenv load_addr "0x6000000"
 
 echo "setting boot args"
-setenv bootargs "root=/dev/mmcblk${devnum}p2 console=ttyS2,1500000n8 rw rootwait loglevel=8"
+setenv bootargs "root=/dev/mmcblk${devnum}p2 console=ttyS2,1500000n8 rw rootwait loglevel=8 init=/etc/overlay_init"
 fatload mmc ${devnum}:${distro_bootpart} ${fdt_addr_r} ${fdtfile}
 fatload mmc ${devnum}:${distro_bootpart} ${kernel_addr_r} Image
 
