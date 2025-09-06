7069228566b03a2ec6020d3f6b6ec7a47505f2a0
PugzAreCute <me@pugzarecute.com>
2025-09-06 14:40:54 +0530

    radxa03w: drop jank DTB copying

--- external/board/radxa03w/boot.cmd
+++ external/board/radxa03w/boot.cmd
@@ -2,7 +2,7 @@ setenv load_addr "0x6000000"
 
 echo "setting boot args"
 setenv bootargs "root=/dev/mmcblk${devnum}p2 console=ttyS2,1500000n8 rw rootwait loglevel=8 init=/etc/overlay_init"
-fatload mmc ${devnum}:${distro_bootpart} ${fdt_addr_r} ${fdtfile}
+fatload mmc ${devnum}:${distro_bootpart} ${fdt_addr_r} rk3566-radxa-zero-3w.dtb
 fatload mmc ${devnum}:${distro_bootpart} ${kernel_addr_r} Image
 
 echo booting linux ...
--- external/board/radxa03w/post-image.sh
+++ external/board/radxa03w/post-image.sh
@@ -4,8 +4,5 @@ set -u
 set -e
 set -x
 
-# DTBs
-mkdir -p $BINARIES_DIR/rockchip
-cp -a $BINARIES_DIR/*.dtb $BINARIES_DIR/rockchip
 support/scripts/genimage.sh -c $BR2_EXTERNAL_AA_PROXY_OS_PATH/board/radxa03w/genimage.cfg
 
