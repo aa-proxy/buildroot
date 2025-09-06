b6ee433c70c1662479b314ac8dd771514142da36
PugzAreCute <me@pugzarecute.com>
2025-09-06 10:08:43 +0530

    radxa03w: drop unnecessary DTBO copy step
    * we don't even build kernel with DTBO support

--- external/board/radxa03w/post-image.sh
+++ external/board/radxa03w/post-image.sh
@@ -4,13 +4,6 @@ set -u
 set -e
 set -x
 
-# DTBOs, although we dont use any
-LINUX_DIR=`find $BASE_DIR/build -name 'vmlinux' -type f | xargs dirname`
-mkdir -p $BINARIES_DIR/rockchip/overlays
-if [ -d ${LINUX_DIR}/arch/arm64/boot/dts/rockchip/overlay ]; then
-  cp -a ${LINUX_DIR}/arch/arm64/boot/dts/rockchip/overlay/*.dtbo $BINARIES_DIR/rockchip/overlays
-fi
-
 # DTBs
 mkdir -p $BINARIES_DIR/rockchip
 cp -a $BINARIES_DIR/*.dtb $BINARIES_DIR/rockchip
