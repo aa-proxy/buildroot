eddb15ceb16b6edddc118bceef882439f539a65c
Mariusz Bialonczyk <manio@skyboo.net>
2025-11-15 15:07:33 +0100

    milkv/uboot: generate needed files and link with build directory

--- external/board/milkv-duos/pre-build.sh
+++ external/board/milkv-duos/pre-build.sh
@@ -8,6 +8,8 @@ set -x
 MEM_DEFS="${BR2_EXTERNAL_AA_PROXY_OS_PATH}/board/milkv-duos/memmap.py"
 SCRIPTS_DIR="${BR2_EXTERNAL_AA_PROXY_OS_PATH}/scripts"
 OUT_MEMMAP_DIR="${BINARIES_DIR}/memmap"
+PARTITION_XML="${BR2_EXTERNAL_AA_PROXY_OS_PATH}/board/milkv-duos/partition/partition_sd.xml"
+BUILDROOT_DIR="$(realpath "$BR2_EXTERNAL_AA_PROXY_OS_PATH/..")"
 
 # Generate memory maps in different formats for future use
 mkdir -p "${OUT_MEMMAP_DIR}"
@@ -15,3 +17,7 @@ python3 ${SCRIPTS_DIR}/mmap_conv.py --type h    ${MEM_DEFS} ${OUT_MEMMAP_DIR}/cv
 python3 ${SCRIPTS_DIR}/mmap_conv.py --type conf ${MEM_DEFS} ${OUT_MEMMAP_DIR}/cvi_board_memmap.conf
 python3 ${SCRIPTS_DIR}/mmap_conv.py --type ld   ${MEM_DEFS} ${OUT_MEMMAP_DIR}/cvi_board_memmap.ld
 python3 ${SCRIPTS_DIR}/memory_display.py        ${MEM_DEFS} ${OUT_MEMMAP_DIR}/cvi_board_memmap.txt
+
+# Generate some magic U-Boot stuff
+python3 ${BUILDROOT_DIR}/tools/image_tool/mkcvipart.py $PARTITION_XML ${OUT_MEMMAP_DIR}
+python3 ${BUILDROOT_DIR}/tools/image_tool/mk_imgHeader.py $PARTITION_XML ${OUT_MEMMAP_DIR}
