864d3effb497f000004655d5d2f2f9f2eaac9a2e
Mariusz Bialonczyk <manio@skyboo.net>
2025-11-20 09:56:19 +0100

    milkv: fix linking Rust binaries
    Because of Sophgo's toolchain incompatibility with Rust musl target we
    need to use separate toolchain to be able to link the binaries
    
    TARGET_CONFIGURE_OPTS from:
    https://github.com/buildroot/buildroot/blob/56a1c162c552be76d8224cb3d96a52214d2ea341/package/Makefile.in#L290

--- external/board/milkv-duos/pre-build.sh
+++ external/board/milkv-duos/pre-build.sh
@@ -21,3 +21,12 @@ python3 ${SCRIPTS_DIR}/memory_display.py        ${MEM_DEFS} ${OUT_MEMMAP_DIR}/cv
 # Generate some magic U-Boot stuff
 python3 ${BUILDROOT_DIR}/tools/image_tool/mkcvipart.py $PARTITION_XML ${OUT_MEMMAP_DIR}
 python3 ${BUILDROOT_DIR}/tools/image_tool/mk_imgHeader.py $PARTITION_XML ${OUT_MEMMAP_DIR}
+
+# We need to use this specific toolchain for building Rust binaries.
+# The reason is that we cannot rely on the system toolchain from Sophgo,
+# because compiling OpenSSL fails with errors like:
+#     crypto/riscv64cpuid.s:67: Error: unknown CSR `vlenb`
+# This toolchain is compatible both with our Buildroot host
+# and with the Rust-supported riscv64-musl target.
+wget https://github.com/riscv-collab/riscv-gnu-toolchain/releases/download/2023.09.27/riscv64-musl-ubuntu-20.04-gcc-nightly-2023.09.27-nightly.tar.gz -P /tmp
+tar -xzf /tmp/riscv64-musl-ubuntu-20.04-gcc-nightly-2023.09.27-nightly.tar.gz -C "${BUILD_DIR}"
--- external/package/aa-proxy-wican/aa-proxy-wican.mk
+++ external/package/aa-proxy-wican/aa-proxy-wican.mk
@@ -17,4 +17,32 @@ AA_PROXY_WICAN_CARGO_ENV = \
     AA_PROXY_WICAN_COMMIT="$(AA_PROXY_WICAN_COMMIT)" \
     BUILDROOT_COMMIT="$(BUILDROOT_COMMIT)"
 
+# use own toolchain only for RISC-V builds (milkv-duos)
+ifeq ($(findstring milkv-duos,$(CONFIG_DIR)),milkv-duos)
+# Add our own toolchain to path
+AA_PROXY_WICAN_CARGO_ENV += PATH=/app/buildroot/output/milkv-duos/build/riscv/bin:$(BR_PATH)
+
+# Prepare target build environment with custom toolchain:
+# - Remove references to the system toolchain to ensure the build
+#   uses our own toolchain paths.
+# - Set all relevant compiler and linker flags (CFLAGS, CXXFLAGS, LDFLAGS, etc.)
+#   for both host and target builds.
+TARGET_CONFIGURE_OPTS = \
+        $(TARGET_MAKE_ENV) \
+        CPPFLAGS_FOR_BUILD="$(HOST_CPPFLAGS)" \
+        CFLAGS_FOR_BUILD="$(HOST_CFLAGS)" \
+        CXXFLAGS_FOR_BUILD="$(HOST_CXXFLAGS)" \
+        LDFLAGS_FOR_BUILD="$(HOST_LDFLAGS)" \
+        FCFLAGS_FOR_BUILD="$(HOST_FCFLAGS)" \
+        CPPFLAGS="$(TARGET_CPPFLAGS)" \
+        CFLAGS="$(TARGET_CFLAGS)" \
+        CXXFLAGS="$(TARGET_CXXFLAGS)" \
+        LDFLAGS="$(TARGET_LDFLAGS)" \
+        FCFLAGS="$(TARGET_FCFLAGS)" \
+        FFLAGS="$(TARGET_FCFLAGS)" \
+        PKG_CONFIG="$(PKG_CONFIG_HOST_BINARY)" \
+        STAGING_DIR="$(STAGING_DIR)" \
+        INTLTOOL_PERL=$(PERL)
+endif
+
 $(eval $(cargo-package))
