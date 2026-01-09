ae9e85ebf1f4cba3a366ae47e75e6cc32b1be667
Chiel Prins <chiel@cpeb.it>
2026-01-09 20:31:18 +0100

    wip

--- external/board/common/post-build.sh
+++ external/board/common/post-build.sh
@@ -5,6 +5,6 @@ set -e
 
 ${BR2_EXTERNAL_AA_PROXY_OS_PATH}/board/common/generate-issue.sh
 
-source ${BR2_EXTERNAL_AA_PROXY_OS_PATH}/board/common/add_usb_serial.sh
+#source ${BR2_EXTERNAL_AA_PROXY_OS_PATH}/board/common/add_usb_serial.sh
 
 
