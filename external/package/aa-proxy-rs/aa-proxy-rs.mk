6bed3e11fe84b6ed8c1fefd02f979b574e28b920
Mariusz Bialonczyk <manio@skyboo.net>
2026-01-31 13:38:22 +0100

    aa-proxy-rs package update

--- external/package/aa-proxy-rs/aa-proxy-rs.mk
+++ external/package/aa-proxy-rs/aa-proxy-rs.mk
@@ -10,12 +10,25 @@ AA_PROXY_RS_COMMIT = $(shell git config --global --add safe.directory $(AA_PROXY
 
 define AA_PROXY_RS_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0755 $(@D)/target/$(RUSTC_TARGET_NAME)/release/aa-proxy-rs $(TARGET_DIR)/usr/bin
-    $(INSTALL) -D -m 0644 $(@D)/contrib/config.toml $(TARGET_DIR)/etc/aa-proxy-rs/config.toml
+    $(INSTALL) -D -m 0644 $(@D)/target/release/config.toml $(TARGET_DIR)/etc/aa-proxy-rs/config.toml
+    $(INSTALL) -D -m 0755 $(@D)/contrib/S93aa-proxy-rs $(TARGET_DIR)/etc/init.d
 endef
 
 # pass git hashes as env variables
 AA_PROXY_RS_CARGO_ENV = \
     AA_PROXY_COMMIT="$(AA_PROXY_RS_COMMIT)" \
     BUILDROOT_COMMIT="$(BUILDROOT_COMMIT)"
+
+# use own toolchain only for RISC-V builds (milkv-duos)
+ifeq ($(findstring milkv-duos,$(CONFIG_DIR)),milkv-duos)
+# Add our own toolchain to path
+AA_PROXY_RS_CARGO_ENV += PATH=/app/buildroot/output/milkv-duos/build/riscv/bin:$(BR_PATH)
+endif
+
+# default config file generator
+define AA_PROXY_RS_GENERATE_CONFIG
+    cd $(@D) && env PATH=$${PATH}:$(HOST_DIR)/bin cargo run --release --bin generate_config
+endef
+AA_PROXY_RS_POST_BUILD_HOOKS += AA_PROXY_RS_GENERATE_CONFIG
 
 $(eval $(cargo-package))
