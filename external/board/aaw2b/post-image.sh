b92827874ec932d6247c6009aac2842f67996232
Chiel Prins <chiel@cpeb.it>
2026-02-01 19:46:53 +0100

    aaw: Add SWUpdate OTA updates support

--- external/board/aaw2b/post-image.sh
+++ external/board/aaw2b/post-image.sh
@@ -3,3 +3,5 @@
 set -e
 
 support/scripts/genimage.sh -c "${BR2_EXTERNAL_AA_PROXY_OS_PATH}/board/aaw2b/genimage.cfg"
+
+swugenerator -o "${BINARIES_DIR}/update_image.swu" -a "${BINARIES_DIR}" -s "${BR2_EXTERNAL_AA_PROXY_OS_PATH}/board/aaw2b/sw-description" -e create
--- external/board/aaw3/post-image.sh
+++ external/board/aaw3/post-image.sh
@@ -4,5 +4,7 @@ set -e
 
 support/scripts/genimage.sh -c "${BR2_EXTERNAL_AA_PROXY_OS_PATH}/board/aaw3/genimage.cfg"
 
-ln -sf boot.img ${BINARIES_DIR}/boot1.img
-ln -sf boot.img ${BINARIES_DIR}/boot2.img
+ln -sf boot.img "${BINARIES_DIR}/boot1.img"
+ln -sf boot.img "${BINARIES_DIR}/boot2.img"
+
+swugenerator -o "${BINARIES_DIR}/update_image.swu" -a "${BINARIES_DIR}" -s "${BR2_EXTERNAL_AA_PROXY_OS_PATH}/board/aaw3/sw-description" -e create
