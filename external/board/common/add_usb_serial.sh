66750fc2c29a508a59f698905b0b48f9317d7175
Mariusz Bialonczyk <manio@skyboo.net>
2026-02-02 07:53:57 +0100

    add_usb_serial.sh: skip for AAW
    CONFIG_VT missing in kernel config so hack to silent getty errors
    is not possible and serial console is spammed with:
    `can't open /dev/ttyGS0: No such device or address`

--- external/board/common/add_usb_serial.sh
+++ external/board/common/add_usb_serial.sh
@@ -3,9 +3,12 @@
 set -u
 set -e
 
-# Add a console on ttyGS0
-if [ -e ${TARGET_DIR}/etc/inittab ]; then
-    grep -qE '^ttyGS0::' ${TARGET_DIR}/etc/inittab || \
-        sed -i '/GENERIC_SERIAL/a\
+# Skip for AAW (CONFIG_VT missing in kernel config so workaround to silent getty errors is not possible)
+if ! grep -q 'aaw' "${BR2_CONFIG}"; then
+    # Add a console on ttyGS0
+    if [ -e ${TARGET_DIR}/etc/inittab ]; then
+        grep -qE '^ttyGS0::' ${TARGET_DIR}/etc/inittab || \
+            sed -i '/GENERIC_SERIAL/a\
 ttyGS0::respawn:/sbin/getty -L ttyGS0 115200 vt100 # usb serial' ${TARGET_DIR}/etc/inittab
+    fi
 fi
