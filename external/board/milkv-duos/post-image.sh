9b8326cc0cfa9044023237987af0e756e6756c71
Mariusz Bialonczyk <manio@skyboo.net>
2026-09-17 16:54:02 +0200

    milkv-duos/post-image.sh: add a case for creating eMMC

--- external/board/milkv-duos/post-image.sh
+++ external/board/milkv-duos/post-image.sh
@@ -27,10 +27,33 @@ lzma -c -9 -f -k Image > Image.lzma
 cp ${BR2_EXTERNAL_AA_PROXY_OS_PATH}/board/milkv-duos/u-boot/multi.its .
 ${BUILD_DIR}/uboot*/tools/mkimage -f multi.its -r boot.itb
 mkdir -p rawimages output
-cp boot.itb rawimages/boot.sd
 
-# create a logo partition
-python3 /app/tools/image_tool/raw2cimg.py ${BR2_EXTERNAL_AA_PROXY_OS_PATH}/board/milkv-duos/bootlogo/logo.jpg ./output $PARTITION_XML
+case "${BOARD}" in
+    *-emmc)
+        # Do not create upgrade.zip here. Produce the individual CIMG images
+        # required by Milk-V's eMMC flashing tools instead.
 
-# make a final SD card image
-${BASE_DIR}/../../support/scripts/genimage.sh -c $BR2_EXTERNAL_AA_PROXY_OS_PATH/board/milkv-duos/genimage.cfg
+        cp boot.itb rawimages/boot.emmc
+        cp rootfs.ext4 rawimages/rootfs_ext4.emmc
+
+        # Create CIMG images and collect all files needed by the `usb_dl` flashing tool.
+        python3 /app/tools/image_tool/raw2cimg.py rawimages/boot.emmc ./output $PARTITION_XML
+        python3 /app/tools/image_tool/raw2cimg.py rawimages/rootfs_ext4.emmc ./output $PARTITION_XML
+        python3 /app/tools/image_tool/raw2cimg.py ${BR2_EXTERNAL_AA_PROXY_OS_PATH}/board/milkv-duos/bootlogo/logo.jpg ./output $PARTITION_XML
+
+        # FIP image
+        cp fip.bin ./output
+
+        # Partition definition
+        cp ${PARTITION_XML} ./output
+        ;;
+    *)
+        cp boot.itb rawimages/boot.sd
+
+        # create a logo partition
+        python3 /app/tools/image_tool/raw2cimg.py ${BR2_EXTERNAL_AA_PROXY_OS_PATH}/board/milkv-duos/bootlogo/logo.jpg ./output $PARTITION_XML
+
+        # make a final SD card image
+        ${BASE_DIR}/../../support/scripts/genimage.sh -c $BR2_EXTERNAL_AA_PROXY_OS_PATH/board/milkv-duos/genimage.cfg
+        ;;
+esac
